b524b4a9a399dbfca572789e761fb37f2684912c
Dhruv Vaghela <dhruvvaghela17@gmail.com>
2023-04-13 14:53:13 -0400

    (ui) - Added tailwind and react-dom-router

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,13 +1,76 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { NotFound404 } from "./components/NotFound404";
+import { Feed } from "./components/Feed";
+import { Interview } from "./components/Interview.js";
+import { TestYourself } from "./components/TestYourself";
+import { Blog } from "./components/Blog";
+import { Trending } from "./components/Trending";
+import { Profile } from "./components/Profile";
+import { Login } from "./components/Login";
+import { Signup } from "./components/Signup";
+import { Landing } from "./components/Landing";
+import { Register } from "./components/Register";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+    errorElement: <NotFound404></NotFound404>,
+    children: [
+      {
+        path: "/",
+        element: <Landing />,
+      },
+      {
+        path: "feed",
+        element: <Feed />,
+      },
+      {
+        path: "interview",
+        element: <Interview />,
+      },
+      {
+        path: "test-yourself",
+        element: <TestYourself />,
+      },
+      {
+        path: "blog",
+        element: <Blog />,
+      },
+      {
+        path: "trending",
+        element: <Trending />,
+      },
+      {
+        path: "profile",
+        element: <Profile />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "signup",
+        element: <Signup />,
+      },
+      {
+        path: "register",
+        element: <Register />,
+      },
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>
 );
 
