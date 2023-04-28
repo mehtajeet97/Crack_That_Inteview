e48cc404dfc729767971653104be24d4979e9c9d
Jeet Mehta <mehta.jeet97@gmail.com>
2023-04-27 20:34:11 -0400

    (ui) -  interview calender currently with errors

--- ui/src/components/Interview.js
+++ ui/src/components/Interview.js
@@ -1,25 +1,32 @@
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import { useState } from "react";
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+const localizer = momentLocalizer(moment);
 export const Interview = () => {
+  const [selectedInterviewTime, setSelectedInterviewTime] = useState(null);
+  const handleInterviewTimeSelection = (time) => {
+    setSelectedInterviewTime(time);
+  };
   return (
-    <div className="bg-cyan-300 rounded overflow-hidden shadow-lg w-full">
-      <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">Interviews</div>
-        <p className="text-gray-700 text-base">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
-          quia, nulla! Maiores et perferendis eaque, exercitationem praesentium
-          nihil.
-        </p>
-      </div>
-      <div className="px-6 pt-4 pb-2">
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-          #photography
-        </span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-          #travel
-        </span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-          #winter
-        </span>
-      </div>
-    </div>
+    <Calendar
+      localizer={localizer}
+      events={[
+        {
+          start: new Date("2023-05-03T10:00:00"),
+          end: new Date("2023-05-03T11:00:00"),
+          title: "Interview with Jeet Mehta",
+        },
+        {
+          start: new Date("2023-05-03T12:00:00"),
+          end: new Date("2023-05-03T13:00:00"),
+          title: "Interview with Interviewer",
+        },
+      ]}
+      startAccessor="start"
+      endAccessor="end"
+      style={{ height: 500 }}
+    />
   );
 };
